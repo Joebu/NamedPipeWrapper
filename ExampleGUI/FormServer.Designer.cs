7571eeb027d841588234f60c7e8da058f1738738
yossi <yossi.buch@gmail.com>
2021-02-23 11:03:54 -0500

    init commit

---
+++ ExampleGUI/FormServer.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ExampleGUI
+{
+    partial class FormServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.richTextBoxMessages = new System.Windows.Forms.RichTextBox();
+            this.listBoxClients = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Client Messages:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 407);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Publish to clients:";
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMessage.Location = new System.Drawing.Point(125, 404);
+            this.textBoxMessage.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(375, 23);
+            this.textBoxMessage.TabIndex = 3;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSend.Location = new System.Drawing.Point(507, 402);
+            this.buttonSend.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(88, 27);
+            this.buttonSend.TabIndex = 4;
+            this.buttonSend.Text = "&Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(14, 14);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBoxMessages);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listBoxClients);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Size = new System.Drawing.Size(581, 381);
+            this.splitContainer1.SplitterDistance = 381;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // richTextBoxMessages
+            // 
+            this.richTextBoxMessages.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxMessages.Location = new System.Drawing.Point(0, 15);
+            this.richTextBoxMessages.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.richTextBoxMessages.Name = "richTextBoxMessages";
+            this.richTextBoxMessages.ReadOnly = true;
+            this.richTextBoxMessages.Size = new System.Drawing.Size(381, 366);
+            this.richTextBoxMessages.TabIndex = 0;
+            this.richTextBoxMessages.Text = "";
+            // 
+            // listBoxClients
+            // 
+            this.listBoxClients.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxClients.FormattingEnabled = true;
+            this.listBoxClients.ItemHeight = 15;
+            this.listBoxClients.Location = new System.Drawing.Point(0, 15);
+            this.listBoxClients.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.listBoxClients.Name = "listBoxClients";
+            this.listBoxClients.Size = new System.Drawing.Size(195, 366);
+            this.listBoxClients.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Clients:";
+            // 
+            // FormServer
+            // 
+            this.AcceptButton = this.buttonSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 442);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FormServer";
+            this.Text = "Server";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBoxMessages;
+        private System.Windows.Forms.ListBox listBoxClients;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ README.md
@@ -0,0 +1,62 @@
+# Named Pipe Wrapper for .Net 5.0
+
+## The package was forked from NamedPipeWrapper 1.4.0 creadted by Andrew C. Dvorak and the original project url is https://github.com/acdvorak/named-pipe-wrapper.
+
+A simple, easy to use, strongly-typed wrapper around .NET 5.0 named pipes.
+
+# NuGet Package
+
+Available as a [NuGet package](https://www.nuget.org/packages/NamedPipeWrapper/).
+
+# Features
+
+- Create named pipe servers that can handle multiple client connections simultaneously.
+- Send strongly-typed messages between clients and servers: any serializable .NET object can be sent over a pipe and will be automatically serialized/deserialized, including cyclical references and complex object graphs.
+- Messages are sent and received asynchronously on a separate background thread and marshalled back to the calling thread (typically the UI).
+- Supports large messages - up to 300 MiB.
+
+# Usage
+
+Server:
+
+```csharp
+var server = new NamedPipeServer<SomeClass>("MyServerPipe");
+
+server.ClientConnected += delegate(NamedPipeConnection<SomeClass> conn)
+    {
+        Console.WriteLine($"Client {conn.Id} is now connected!");
+        conn.PushMessage(new SomeClass { Text: "Welcome!" });
+    };
+
+server.ClientMessage += delegate(NamedPipeConnection<SomeClass> conn, SomeClass message)
+    {
+        Console.WriteLine($"Client {conn.Id} says: {message.Text}");
+    };
+
+// Start up the server asynchronously and begin listening for connections.
+// This method will return immediately while the server runs in a separate background thread.
+server.Start();
+
+// ...
+```
+
+Client:
+
+```csharp
+var client = new NamedPipeClient<SomeClass>("MyServerPipe");
+
+client.ServerMessage += delegate(NamedPipeConnection<SomeClass> conn, SomeClass message)
+    {
+        Console.WriteLine($"Server says: {message.Text}");
+    };
+
+// Start up the client asynchronously and connect to the specified server pipe.
+// This method will return immediately while the client runs in a separate background thread.
+client.Start();
+
+// ...
+```
+
+# MIT License
+
+Named Pipe Wrapper for .NET is licensed under the [MIT license](LICENSE.txt).
